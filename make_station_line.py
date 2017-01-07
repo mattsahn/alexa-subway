07d96f330332d044de30161776837fac5e32409c
Matt Sahn <sahn44@yahoo.com>
2017-01-07 17:59:22 +0000

    script to generate station/line info used for station lookup

---
+++ make_station_line.py
@@ -0,0 +1,17 @@
+import requests
+import json
+mta_api_url = "https://pbdexmgg8g.execute-api.us-east-1.amazonaws.com/dev"
+
+# submitting request to get list of train lines
+MTARequest = requests.get(mta_api_url+"/routes")
+
+data = json.loads(MTARequest.text)
+
+stations = []
+
+# iterate over each train line and get associated station IDs
+for line in data['data']:
+    MTARequest = requests.get(mta_api_url+"/by-route/" + line)
+    data = json.loads(MTARequest.text)
+    for station in data['data']:
+        print(line + "|" + str(station['id']))
