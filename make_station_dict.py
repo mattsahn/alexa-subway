c1fe9c57efb9d126913ceba8dc01c5896ef42961
Matt Sahn <sahn44@yahoo.com>
2017-05-14 02:05:41 +0000

    updated STATION custom slot values and added station dict making script

---
+++ make_station_dict.py
@@ -0,0 +1,17 @@
+import requests
+import json
+mta_api_url = "https://rcgj41hz63.execute-api.us-east-1.amazonaws.com/v2"
+
+# submitting request to get list of train lines
+MTARequest = requests.get(mta_api_url+"/routes")
+
+data = json.loads(MTARequest.text)
+
+stations = []
+
+# iterate over each train line and get associated station IDs
+for line in data['data']:
+    MTARequest = requests.get(mta_api_url+"/by-route/" + line)
+    data = json.loads(MTARequest.text)
+    for station in data['data']:
+        print(str(station['id']) + "|" + str(station['name']).lower() )
