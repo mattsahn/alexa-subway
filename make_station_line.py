18e708de111f187e9bba4b60c91a4586404ec20c
Matt Sahn <sahn44@yahoo.com>
2018-01-10 00:16:24 +0000

    refreshed StationLine file. Had wrong F train data. fix to make_station_line.py to handle empty train element in response

--- make_station_line.py
+++ make_station_line.py
@@ -4,13 +4,13 @@ mta_api_url = "https://rcgj41hz63.execute-api.us-east-1.amazonaws.com/v2"
 
 # submitting request to get list of train lines
 MTARequest = requests.get(mta_api_url+"/routes")
-
 data = json.loads(MTARequest.text)
-
 stations = []
 
 # iterate over each train line and get associated station IDs
 for line in data['data']:
+    if(str(line) == ''):
+        continue
     MTARequest = requests.get(mta_api_url+"/by-route/" + line)
     data = json.loads(MTARequest.text)
     for station in data['data']:
